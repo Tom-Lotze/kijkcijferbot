fec5bfc5c7a2d6bd099ed0e0832726d87fb50775
Tom Lotze <tom.lotze@student.auc.nl>
2020-02-18 18:53:44 +0100

    website is crawled, and information scraped

---
+++ tweet_bot.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# python 3
+# @Author: Tom Lotze
+# @Date:   2020-02-18 18:49:26
+# @Last Modified by:   Tom Lotze
+# @Last Modified time: 2020-02-18 18:52:10
+
+import tweepy
+import requests
+from bs4 import BeautifulSoup as BS
+from scrape_kijkcijfers import *
+
+top = get_top()
+
+
+send_tweet(msg)
+
