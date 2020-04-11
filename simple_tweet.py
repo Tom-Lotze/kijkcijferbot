5954a71e7f9540de2c4e936fb020954a78142b39
Tom Lotze <tom.lotze@student.auc.nl>
2020-04-11 15:02:18 +0200

    media tweet can be send

--- simple_tweet.py
+++ simple_tweet.py
@@ -1,17 +1,30 @@
-# importing tweepy module 
-import tweepy 
+# importing tweepy module
+import tweepy
 from get_keys import *
-  
-# personal details 
+
+# personal details
 consumer_key, consumer_secret, access_token, access_token_secret = get_personal_keys()
-  
+
 def send_tweet(msg):
-    # authentication of consumer key and secret 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
-      
-    # authentication of access token and secret 
-    auth.set_access_token(access_token, access_token_secret) 
-    api = tweepy.API(auth) 
-      
-    # update the status 
-    api.update_status(status=msg) 
+    # authentication of consumer key and secret
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+    # authentication of access token and secret
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    # update the status
+    api.update_status(status=msg)
+
+
+def send_media_tweet(imagePath, status):
+    # authentication of consumer key and secret
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+    # authentication of access token and secret
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    # update the status
+    api.update_with_media(imagePath, status)
+
