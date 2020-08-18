99307fd21778b8c158de21636052ac19e228a67f
Berend Jansen <bereeend@gmail.com>
2020-08-18 13:53:06 +0200

    fixed scraper for new website layout

--- scrape_kijkcijfers.py
+++ scrape_kijkcijfers.py
@@ -10,11 +10,13 @@ def get_top(url="https://kijkonderzoek.nl/"):
 
     # extract titles and viewing numbers
     titles = html.find_all("td", class_="kc_cdtitle", limit=25)
-    viewings = html.find_all("td", class_="kc_cdrt0", limit=25)
+    viewings = html.find_all("td", class_="kc_cdrt0", limit=50)
 
+    avg_viewings = [viewings[i] for i in range(0,50,2)]
+    
     # construct a ranking
-    ranking = [(title.div.string, number.string) for title, number in zip(titles, viewings)]
-
+    ranking = [(title.div.string, number.string) for title, number in zip(titles, avg_viewings)]
+    
     return ranking
 
-
+print(get_top())
